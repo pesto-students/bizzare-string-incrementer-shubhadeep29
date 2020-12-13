c48a18bc9f850f0c0b773edadbc4924c190e6317
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2020-12-14 01:13:34 +0530

    initial commit

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,38 @@
 // Start your implementation here
+
+const bizarreStringIncrementer = (bizarreString) => {
+    let resultStr = '';
+    for (const digit of getStringAndNumber(bizarreString).lastNumbers) {
+        if (isNumber(digit)) {
+            resultStr +=digit
+        } else {
+            break
+        }
+    }
+
+    return getStringAndNumber(bizarreString).firstString.join('') + ( Number(resultStr)+1);
+
+};
+
+let getStringAndNumber = (bizarreString) => {
+    const reversedSplittedString = bizarreString.split('').reverse();
+    let numberPositionCount = 0;
+    for (const letters of reversedSplittedString) {
+        if (isNumber(letters)) {
+            numberPositionCount++
+        } else {
+            break
+        }
+    }
+    lastNumbers = reversedSplittedString.slice(0, numberPositionCount).reverse()
+    firstString = reversedSplittedString.slice(numberPositionCount, numberPositionCount.length).reverse()
+    //console.log(resultSum,numberPositionCount,firstString,lastNumbers);
+    return {'firstString': firstString, 'lastNumbers': lastNumbers};
+}
+
+let isNumber = (letter) => {
+    var pattern = /^\d+$/;
+    return pattern.test(letter);  // returns a boolean
+}
+
+module.exports =  bizarreStringIncrementer 
