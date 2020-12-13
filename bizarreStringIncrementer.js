c8ead56b33c4cd524c01e8af32a77ad7ef71341b
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2020-12-14 01:15:00 +0530

    some changes

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,38 +1,40 @@
 // Start your implementation here
 
 const bizarreStringIncrementer = (bizarreString) => {
-    let resultStr = '';
-    for (const digit of getStringAndNumber(bizarreString).lastNumbers) {
-        if (isNumber(digit)) {
-            resultStr +=digit
-        } else {
-            break
-        }
+  let resultStr = "";
+  for (const digit of getStringAndNumber(bizarreString).lastNumbers) {
+    if (isNumber(digit)) {
+      resultStr += digit;
+    } else {
+      break;
     }
-
-    return getStringAndNumber(bizarreString).firstString.join('') + ( Number(resultStr)+1);
-
+  }
+  return (
+    getStringAndNumber(bizarreString).firstString.join("") +
+    (Number(resultStr) + 1)
+  );
 };
 
 let getStringAndNumber = (bizarreString) => {
-    const reversedSplittedString = bizarreString.split('').reverse();
-    let numberPositionCount = 0;
-    for (const letters of reversedSplittedString) {
-        if (isNumber(letters)) {
-            numberPositionCount++
-        } else {
-            break
-        }
+  const reversedSplittedString = bizarreString.split("").reverse();
+  let numberPositionCount = 0;
+  for (const letters of reversedSplittedString) {
+    if (isNumber(letters)) {
+      numberPositionCount++;
+    } else {
+      break;
     }
-    lastNumbers = reversedSplittedString.slice(0, numberPositionCount).reverse()
-    firstString = reversedSplittedString.slice(numberPositionCount, numberPositionCount.length).reverse()
-    //console.log(resultSum,numberPositionCount,firstString,lastNumbers);
-    return {'firstString': firstString, 'lastNumbers': lastNumbers};
-}
+  }
+  lastNumbers = reversedSplittedString.slice(0, numberPositionCount).reverse();
+  firstString = reversedSplittedString
+    .slice(numberPositionCount, numberPositionCount.length)
+    .reverse();
+  return { firstString: firstString, lastNumbers: lastNumbers };
+};
 
 let isNumber = (letter) => {
-    var pattern = /^\d+$/;
-    return pattern.test(letter);  // returns a boolean
-}
+  var pattern = /^\d+$/;
+  return pattern.test(letter);
+};
 
-module.exports =  bizarreStringIncrementer 
+module.exports = bizarreStringIncrementer;
